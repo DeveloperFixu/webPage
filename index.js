29c17c9d08e04ffde47c12cf8caf468303de0f54
ITBF BUSSINES <67914239+DeveloperFixu@users.noreply.github.com>
2021-01-19 09:54:12 -0500

    Add js

---
+++ index.js
@@ -0,0 +1,32 @@
+function filtro(c) {
+    var x = document.getElementById("gallery");
+    var y = x.querySelectorAll("div");
+    var i,c;
+    for (i = 0; i < y.length; i++) {
+    if (y[i].id == c || c == "all") {y[i].style.display = "grid"}
+    else {y[i].style.display = "none"}
+}
+}
+
+const mobile = window.matchMedia('screen and (max-width: 767px)');
+      const menu = document.querySelector('.menu');
+     
+      const burgerButton = document.querySelector('#burger-menu');
+      mobile.addListener(validation)
+      function validation(event) {        
+        if(event.matches) {
+          burgerButton.addEventListener('click',hideShow)
+        }
+        else {
+          burgerButton.removeEventListener('click',hideShow)
+        }
+      } 
+      validation(mobile);     
+      function hideShow() {
+        if(menu.classList.contains('is-active')){
+          menu.classList.remove('is-active'); 
+        }
+        else{          
+          menu.classList.add('is-active'); 
+        }
+      }
