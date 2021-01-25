b86418049c94e7ff57e3ba648bee83d9b1624796
ITBF BUSSINES <67914239+DeveloperFixu@users.noreply.github.com>
2021-01-25 11:03:01 -0500

    Sección Comentarios

--- index.js
+++ index.js
@@ -31,11 +31,13 @@
 //         }
 //       }
 
+      // Altura Header
+
       $(document).ready(function(){
-         var altura = $('.carousel').offset().top; 
+         var altura = $('#carousel').offset().top; 
         //  alert(altura); 
         $(window).on('scroll', function(){
-            if( $(window).scrollTop() > altura ){
+            if( $(window).scrollTop() > (altura -1) ){
                 $('.header').addClass('menu-fixed animated fadeIn'); 
             }else{
                 $('.header').removeClass('menu-fixed');
@@ -44,6 +46,83 @@
         }); 
       }); 
 
+    // Modal
+
       $('#ventanaModal').on('shown.bs.modal', function () { 
         $('#myInput').trigger('focus')
       })
+
+      window.onload = () => {
+        document.querySelector(".arrow-right").addEventListener("click", clickRight);
+        document.querySelector(".arrow-left").addEventListener("click", clickLeft);
+        document
+          
+          // .addEventListener("click", showNotification);
+        document.querySelectorAll(".project").forEach(element => {
+          element.addEventListener("click", e => openModal(e));
+        });
+        // document.body.addEventListener("click", e => closeModal(e));
+        document.body.addEventListener("keyup", e => listenForEsc(e)); 
+      };
+
+      // Comentarios
+      
+      /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
+      function clickRight() {
+        const currentLeft = parseInt(
+          getComputedStyle(document.querySelector(".project-container")).left,
+          10
+        );
+        if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
+          return;
+        }
+        let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
+        document.querySelector(".project-container").style.left = `${newValue}px`;
+        switch(newValue){
+          case -270:
+            document.querySelector('.project1').setAttribute("tabindex", "-1");
+            document.querySelector('.project1-container').setAttribute("aria-hidden", true); 
+            document.querySelector('.project4').removeAttribute("tabindex");
+            document.querySelector('.project4-container').removeAttribute("aria-hidden"); 
+            break;
+          case -540:
+            document.querySelector('.project2').setAttribute("tabindex", "-1");
+            document.querySelector('.project2-container').setAttribute("aria-hidden", true);
+            document.querySelector('.project5').removeAttribute("tabindex");
+            document.querySelector('.project5-container').removeAttribute("aria-hidden");
+            break;
+          default:
+            break;
+        }
+      }
+      
+      /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
+      function clickLeft() {
+        const currentLeft = parseInt(
+          getComputedStyle(document.querySelector(".project-container")).left,
+          10
+        );
+        if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
+          return;
+        }
+        let newValue = currentLeft + 270;
+        document.querySelector(".project-container").style.left = `${newValue}px`;
+      
+        switch(newValue){
+          case -270:
+            document.querySelector('.project5').setAttribute("tabindex", "-1"),
+            document.querySelector('.project5-container').setAttribute("aria-hidden", true);
+            document.querySelector('.project2').removeAttribute("tabindex"); 
+            document.querySelector('.project2-container').removeAttribute("aria-hidden"); 
+            break;
+          case 0:
+            document.querySelector('.project4').setAttribute("tabindex", "-1");
+            document.querySelector('.project4-container').setAttribute("aria-hidden", true);
+            document.querySelector('.project1').removeAttribute("tabindex");
+            document.querySelector('.project1-container').removeAttribute("aria-hidden");
+            break;
+          default:
+            break;
+        }
+      }
+      
