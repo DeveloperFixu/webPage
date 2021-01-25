4fbd6cfd614137222dee2c3c93e44d56a1271012
ITBF BUSSINES <67914239+DeveloperFixu@users.noreply.github.com>
2021-01-25 17:40:17 -0500

    Sección Especialistas

--- index.js
+++ index.js
@@ -126,3 +126,17 @@
         }
       }
       
+      function contador() {
+        const counters = document.querySelectorAll('.counter');
+        const speed = 200;
+        counters.forEach(counter => {
+        const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const inc = target / speed;
+        if (count < target) {
+        counter.innerText = count + inc;
+        setTimeout(updateCount, 1);
+        } else {
+        counter.innerText = target;}};
+        updateCount();});}
