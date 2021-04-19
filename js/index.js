5a0f61fb92332649a92996e433aacf8405de6d5a
LopezDiegoDev <82101634+LopezDiegoDev@users.noreply.github.com>
2021-04-19 15:28:22 -0500

    add js-formulario-css de registro y iconos-Ser.

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@
 
 // const mobile = window.matchMedia('screen and (max-width: 767px)');
 //       const menu = document.querySelector('.menu');
-     
+
 //       const burgerButton = document.querySelector('#burger-menu');
 //       mobile.addListener(validation)
 //       function validation(event) {        
@@ -31,112 +31,116 @@
 //         }
 //       }
 
-      // Altura Header
+// Altura Header
 
-      $(document).ready(function(){
-         var altura = $('#altura').offset().top; 
-        //  alert(altura); 
-        $(window).on('scroll', function(){
-            if( $(window).scrollTop() > (altura -1) ){
-                $('.header').addClass('menu-fixed animated fadeIn'); 
-            }else{
-                $('.header').removeClass('menu-fixed');
-                $('.header').addClass('animated fadeIn');
-            }
-        }); 
-      }); 
+$(document).ready(function () {
+  var altura = $('#altura').offset().top;
+  //  alert(altura); 
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > (altura - 1)) {
+      $('.header').addClass('menu-fixed animated fadeIn');
+    } else {
+      $('.header').removeClass('menu-fixed');
+      $('.header').addClass('animated fadeIn');
+    }
+  });
+});
 
-    // Modal
+// Modal
 
-      $('#ventanaModal').on('shown.bs.modal', function () { 
-        $('#myInput').trigger('focus')
-      })
+$('#ventanaModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
 
-      window.onload = () => {
-        document.querySelector(".arrow-right").addEventListener("click", clickRight);
-        document.querySelector(".arrow-left").addEventListener("click", clickLeft);
-        document
-          
-          // .addEventListener("click", showNotification);
-        document.querySelectorAll(".project").forEach(element => {
-          element.addEventListener("click", e => openModal(e));
-        });
-        // document.body.addEventListener("click", e => closeModal(e));
-        // document.body.addEventListener("keyup", e => listenForEsc(e)); 
-      };
+window.onload = () => {
+  document.querySelector(".arrow-right").addEventListener("click", clickRight);
+  document.querySelector(".arrow-left").addEventListener("click", clickLeft);
+  document
 
-      // Comentarios
-      
-      /** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
-      function clickRight() {
-        const currentLeft = parseInt(
-          getComputedStyle(document.querySelector(".project-container")).left,
-          10
-        );
-        if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
-          return;
-        }
-        let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
-        document.querySelector(".project-container").style.left = `${newValue}px`;
-        switch(newValue){
-          case -270:
-            document.querySelector('.project1').setAttribute("tabindex", "-1");
-            document.querySelector('.project1-container').setAttribute("aria-hidden", true); 
-            document.querySelector('.project4').removeAttribute("tabindex");
-            document.querySelector('.project4-container').removeAttribute("aria-hidden"); 
-            break;
-          case -540:
-            document.querySelector('.project2').setAttribute("tabindex", "-1");
-            document.querySelector('.project2-container').setAttribute("aria-hidden", true);
-            document.querySelector('.project5').removeAttribute("tabindex");
-            document.querySelector('.project5-container').removeAttribute("aria-hidden");
-            break;
-          default:
-            break;
-        }
-      }
-      
-      /** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
-      function clickLeft() {
-        const currentLeft = parseInt(
-          getComputedStyle(document.querySelector(".project-container")).left,
-          10
-        );
-        if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
-          return;
-        }
-        let newValue = currentLeft + 270;
-        document.querySelector(".project-container").style.left = `${newValue}px`;
-      
-        switch(newValue){
-          case -270:
-            document.querySelector('.project5').setAttribute("tabindex", "-1"),
-            document.querySelector('.project5-container').setAttribute("aria-hidden", true);
-            document.querySelector('.project2').removeAttribute("tabindex"); 
-            document.querySelector('.project2-container').removeAttribute("aria-hidden"); 
-            break;
-          case 0:
-            document.querySelector('.project4').setAttribute("tabindex", "-1");
-            document.querySelector('.project4-container').setAttribute("aria-hidden", true);
-            document.querySelector('.project1').removeAttribute("tabindex");
-            document.querySelector('.project1-container').removeAttribute("aria-hidden");
-            break;
-          default:
-            break;
-        }
-      }
-      
-      function contador() {
-        const counters = document.querySelectorAll('.counter');
-        const speed = 200;
-        counters.forEach(counter => {
-        const updateCount = () => {
-        const target = +counter.getAttribute('data-target');
-        const count = +counter.innerText;
-        const inc = target / speed;
-        if (count < target) {
+  // .addEventListener("click", showNotification);
+  document.querySelectorAll(".project").forEach(element => {
+    element.addEventListener("click", e => openModal(e));
+  });
+  // document.body.addEventListener("click", e => closeModal(e));
+  // document.body.addEventListener("keyup", e => listenForEsc(e)); 
+};
+
+// Comentarios
+
+/** Esta funcion se llama cuando la persona hace click en la fecha derecha del carousel para navegar a la derecha */
+function clickRight() {
+  const currentLeft = parseInt(
+    getComputedStyle(document.querySelector(".project-container")).left,
+    10
+  );
+  if (currentLeft < -270) { //si el valor de izquierda es menor a -270, para de mover el contenido
+    return;
+  }
+  let newValue = currentLeft - 270; //270 toma en cuenta el tamaño de la imagen mas sus margines
+  document.querySelector(".project-container").style.left = `${newValue}px`;
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project1').setAttribute("tabindex", "-1");
+      document.querySelector('.project1-container').setAttribute("aria-hidden", true);
+      document.querySelector('.project4').removeAttribute("tabindex");
+      document.querySelector('.project4-container').removeAttribute("aria-hidden");
+      break;
+    case -540:
+      document.querySelector('.project2').setAttribute("tabindex", "-1");
+      document.querySelector('.project2-container').setAttribute("aria-hidden", true);
+      document.querySelector('.project5').removeAttribute("tabindex");
+      document.querySelector('.project5-container').removeAttribute("aria-hidden");
+      break;
+    default:
+      break;
+  }
+}
+
+/** Esta funcion se llama cuando la persona hace click en la fecha izquierda del carousel para navegar a la izquierda */
+function clickLeft() {
+  const currentLeft = parseInt(
+    getComputedStyle(document.querySelector(".project-container")).left,
+    10
+  );
+  if (currentLeft === 0) { //si el valor de izquiera es 0, retornar para no seguir movierno el contenido
+    return;
+  }
+  let newValue = currentLeft + 270;
+  document.querySelector(".project-container").style.left = `${newValue}px`;
+
+  switch (newValue) {
+    case -270:
+      document.querySelector('.project5').setAttribute("tabindex", "-1"),
+        document.querySelector('.project5-container').setAttribute("aria-hidden", true);
+      document.querySelector('.project2').removeAttribute("tabindex");
+      document.querySelector('.project2-container').removeAttribute("aria-hidden");
+      break;
+    case 0:
+      document.querySelector('.project4').setAttribute("tabindex", "-1");
+      document.querySelector('.project4-container').setAttribute("aria-hidden", true);
+      document.querySelector('.project1').removeAttribute("tabindex");
+      document.querySelector('.project1-container').removeAttribute("aria-hidden");
+      break;
+    default:
+      break;
+  }
+}
+
+function contador() {
+  const counters = document.querySelectorAll('.counter');
+  const speed = 200;
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const count = +counter.innerText;
+      const inc = target / speed;
+      if (count < target) {
         counter.innerText = count + inc;
         setTimeout(updateCount, 1);
-        } else {
-        counter.innerText = target;}};
-        updateCount();});}
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+}
