f39a69d6ce4b9116f12ac7297fd00d95dcf38621
LopezDiegoDev <82101634+LopezDiegoDev@users.noreply.github.com>
2021-04-22 15:11:31 -0500

    add css Gid para SobreNosotros.html
    Elimino Js Grid y todo se maneja por Css Grid que es lo que se agrega.

--- js/index.js
+++ js/index.js
@@ -31,6 +31,7 @@
 //         }
 //       }
 
+
 // Altura Header
 
 $(document).ready(function () {
@@ -258,13 +259,3 @@ formulario.addEventListener('submit', (e) => {
     }
 });
 
-/**Js sobre nosotros */
-//Initialize with Vanilla JavaScript
-var elem = document.querySelector('.contenedor_galeria');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.item',
-  columnWidth: 230,
-  gutter: 20,
-  isFitWidth: true
-});
