c334d1040471516b46b52a0e6cfc390f3c7a767d
LopezDiegoDev <82101634+LopezDiegoDev@users.noreply.github.com>
2021-05-25 14:53:07 -0500

    add slider and styles of update index
    Se añade slider antes y despues, además de agregar css twenty y js twenty

--- js/index.js
+++ js/index.js
@@ -32,6 +32,12 @@
 //       }
 
 
+$(function(){
+  $(".twentytwenty-container[data-orientation!='vertical']").twentytwenty({default_offset_pct: 0.7});
+  $(".twentytwenty-container[data-orientation='vertical']").twentytwenty({default_offset_pct: 0.3, orientation: 'vertical'});
+});
+
+
 $("#slider").on("input change", (e)=>{
   const sliderPos = e.target.value;
   // Update the width of the foreground image
